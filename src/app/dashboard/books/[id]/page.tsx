d219f3d63ca8bfaea52ab70abe6967a4f79065b7
Ramy29 <ramyesam701@gmail.com>
2025-09-07 02:35:02 +0300

    Fix/Warning

--- src/app/dashboard/books/[id]/page.tsx
+++ src/app/dashboard/books/[id]/page.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import BookDetails from '../_components/book-details'
-import { getSpecificProduct } from '@/lib/actions/dashboard/getProduct';
+import React from "react";
+import BookDetails from "../_components/book-details";
+import { getSpecificProduct } from "@/lib/actions/dashboard/getProduct";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function page({params}:PageProps) {
-    const {id} =  params
- const bookDetails = await getSpecificProduct(id)
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  const bookDetails = await getSpecificProduct(id);
 
   return (
-    <div><BookDetails bookDetails={bookDetails}/></div>
-  )
+    <div>
+      <BookDetails bookDetails={bookDetails} />
+    </div>
+  );
 }
+
 
--- src/app/dashboard/confimation/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Confirmation from './_components/confirmation'
-
-export default function page() {
-  return (
-    <div><Confirmation/></div>
-  )
-}
